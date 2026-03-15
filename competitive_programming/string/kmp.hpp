68a065ad7d6a96ae6a36fba8748319d6cede43a2
ThiagoFBastos <thiagobastos1705@gmail.com>
2026-03-15 19:21:35 -0300

    feat: add a kmp algorithm implementation

---
+++ competitive_programming/string/kmp.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+inline std::vector<int> kmp(const std::string& s) 
+{
+	int n = s.size();
+	std::vector<int> p(n);
+	p[0] = 0;
+	for(int i = 1; i < n; ++i) {
+		int j = p[i - 1];
+		while(j && s[i] != s[j]) j = p[j - 1];
+		p[i] = j;
+		if(s[i] == s[j]) ++p[i];
+	}
+	return p;
+}
