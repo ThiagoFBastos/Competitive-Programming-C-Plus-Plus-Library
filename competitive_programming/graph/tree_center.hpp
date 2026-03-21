816497c9aee99368cde2bf481d10e72dbc95e76d
ThiagoFBastos <thiagobastos1705@gmail.com>
2026-03-20 21:03:25 -0300

    refactor: rename the tree namespace to graph namespace

--- competitive_programming/graph/tree_center.hpp
+++ competitive_programming/graph/tree_center.hpp
@@ -8,78 +8,80 @@
  * @brief header that compute the center and diameter of a tree
  */
 
-namespace tree
+namespace graph
 {
-    /**
-     * Compute the time until a node becomes a leaf
-     * @param adj the adjacency list of the tree
-     */
-    inline std::vector<int> bfs(const std::vector<std::vector<int>>& adj)
+
+/**
+ * Compute the time until a node becomes a leaf
+ * @param adj the adjacency list of the tree
+ */
+inline std::vector<int> bfs(const std::vector<std::vector<int>>& adj)
+{
+    const auto n = adj.size();
+    std::vector<int> deg(n), dist(n);
+    std::queue<int> q;
+
+    for(std::size_t i = 0; i < n; ++i)
     {
-        const auto n = adj.size();
-        std::vector<int> deg(n), dist(n);
-        std::queue<int> q;
+        deg[i] = adj[i].size();
 
-        for(std::size_t i = 0; i < n; ++i)
+        if(deg[i] <= 1)
         {
-            deg[i] = adj[i].size();
+            q.push(i);
+            dist[i] = 0;
+        }
+    }
 
-            if(deg[i] <= 1)
+    while(!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        for(int v : adj[u])
+        {
+            if(--deg[v] == 1)
             {
-                q.push(i);
-                dist[i] = 0;
+                q.push(v);
+                dist[v] = 1 + dist[u];
             }
         }
-
-        while(!q.empty())
-        {
-            int u = q.front();
-            q.pop();
-
-            for(int v : adj[u])
-            {
-                if(--deg[v] == 1)
-                {
-                    q.push(v);
-                    dist[v] = 1 + dist[u];
-                }
-            }
-        }
-
-        return dist;
     }
 
-    /**
-     * Find the center of a tree
-     * @param adj the adjacency list of the tree
-     */
-    inline std::vector<int> center(const std::vector<std::vector<int>>& adj)
+    return dist;
+}
+
+/**
+ * Find the center of a tree
+ * @param adj the adjacency list of the tree
+ */
+inline std::vector<int> center(const std::vector<std::vector<int>>& adj)
+{
+    const auto dist = bfs(adj);
+
+    int max_dist = *std::max_element(dist.begin(), dist.end());
+
+    std::vector<int> c;
+
+    for(size_t i = 0; i < adj.size(); ++i)
     {
-        const auto dist = bfs(adj);
-
-        int max_dist = *std::max_element(dist.begin(), dist.end());
-
-        std::vector<int> c;
-
-        for(size_t i = 0; i < adj.size(); ++i)
-        {
-            if(dist[i] == max_dist)
-                c.push_back(static_cast<int>(i));
-        }
-
-        return c;
+        if(dist[i] == max_dist)
+            c.push_back(static_cast<int>(i));
     }
 
-    /**
-     * Find the diameter of a tree
-     * @param adj the adjacency list of the tree
-     */
-    inline int diameter(const std::vector<std::vector<int>>& adj)
-    {
-        const auto dist = bfs(adj);
+    return c;
+}
 
-        int max_dist = *std::max_element(dist.begin(), dist.end());
+/**
+ * Find the diameter of a tree
+ * @param adj the adjacency list of the tree
+ */
+inline int diameter(const std::vector<std::vector<int>>& adj)
+{
+    const auto dist = bfs(adj);
 
-        return 2 * max_dist + std::count(dist.begin(), dist.end(), max_dist) - 1;
-    }
+    int max_dist = *std::max_element(dist.begin(), dist.end());
+
+    return 2 * max_dist + std::count(dist.begin(), dist.end(), max_dist) - 1;
+}
+
 }
