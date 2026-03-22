67e7a98b3f2e6198fac82d79d8310db309651be5
Thiago Bastos <thiagobastos1705@gmail.com>
2026-03-21 21:14:04 -0300

    refactor: make some improvements in the binary exponentiation algorithm

--- competitive_programming/src/math/bin_exp.hpp
+++ competitive_programming/src/math/bin_exp.hpp
@@ -2,14 +2,24 @@
 
 namespace math {
 
+/**
+ * @brief Calculate n^p % mod in O(log p)
+ * @param n the base number
+ * @param p the power
+ * @param mod the modulo number
+ */
 inline long long bin_exp(long long n, long long p, long long mod) 
 {
 	long long ans = 1;
+
+	n %= mod;
+
 	for(; p > 0; p >>= 1) 
     {
 		if(p & 1) ans = ans * n % mod;
 		n = n * n % mod;
 	}
+
 	return ans;
 }
 
