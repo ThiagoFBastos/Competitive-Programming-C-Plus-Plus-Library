79f91b0c38acc33b1ce37f8e4008b5d4a581b4f6
Thiago Bastos <thiagobastos1705@gmail.com>
2026-03-21 21:07:55 -0300

    refactor: make some improvements in the code

--- competitive_programming/src/dp/edit_distance.hpp
+++ competitive_programming/src/dp/edit_distance.hpp
@@ -6,20 +6,23 @@
 
 namespace dp {
 
+/**
+ * @brief Calculate the edit distance between two strings
+ */
 inline int edit_distance(const std::string& s, const std::string& t)
 {
     constexpr int INF = 1e9;
-    int n = s.size(), m = t.size();
+    std::size_t n = s.size(), m = t.size();
 	std::vector<std::vector<int>> dp(n + 2);
 
-	for(int k = 0; k <= n + 1; ++k) 
+	for(int k = 0; k <= n + 1; ++k)
         dp[k].assign(m + 2, INF);
 
 	dp[0][0] = 0;
 
-	for(int i = 0; i <= n; ++i) 
+	for(std::size_t i = 0; i <= n; ++i)
     {
-		for(int j = 0; j <= m; ++j) 
+		for(std::size_t j = 0; j <= m; ++j)
         {
 			dp[i + 1][j + 1] = std::min(dp[i + 1][j + 1], dp[i][j] + (t[j] != s[i]));
 			dp[i + 1][j] = std::min(dp[i + 1][j], 1 + dp[i][j]);
