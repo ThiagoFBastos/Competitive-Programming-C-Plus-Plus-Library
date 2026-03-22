8845303477e8e652e4746a6523fd1399f54e3a52
Thiago Bastos <thiagobastos1705@gmail.com>
2026-03-21 21:48:20 -0300

    test: add tests to the z function algorithm

---
+++ competitive_programming/test/string/z_function.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+
+#include "string/z_function.hpp"
+
+TEST(ZFunction, Palindrome)
+{
+    const std::string word = "abcbcba";
+    const std::vector<int> expected {0, 0, 0, 0, 0, 0, 1};
+
+    const auto z = strings::z_function(word);
+
+    EXPECT_EQ(z, expected);
+}
+
+TEST(ZFunction, AllZeroes)
+{
+    const std::string word = "mississippi";
+    const std::vector<int> expected {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    const auto z = strings::z_function(word);
+
+    EXPECT_EQ(z, expected);
+}
+
+TEST(ZFunction, SomeEqualSuffixies)
+{
+    const std::string word = "ababacaca";
+    const std::vector<int> expected {0, 0, 3, 0, 1, 0, 1, 0, 1};
+
+    const auto z = strings::z_function(word);
+
+    EXPECT_EQ(z, expected);
+}
+
+TEST(ZFunction, AllEqual)
+{
+    const std::string word = "aaaaa";
+    const std::vector<int> expected {0, 4, 3, 2, 1};
+
+    const auto z = strings::z_function(word);
+
+    EXPECT_EQ(z, expected);    
+}
