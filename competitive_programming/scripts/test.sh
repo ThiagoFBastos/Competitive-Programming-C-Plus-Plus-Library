54b6b60eba934e21f96ac4e63eebf5bb5dd324eb
ThiagoFBastos <thiagobastos1705@gmail.com>
2026-03-31 20:24:40 -0300

    fix: fix directory doesn't exists

--- competitive_programming/scripts/test.sh
+++ competitive_programming/scripts/test.sh
@@ -4,8 +4,12 @@ set -e
 
 BUILD_DIR=build
 
-echo "==> Compilando"
-cmake --build $BUILD_DIR
+if [ -d $BUILD_DIR ]; then
+    echo "==> Compilando"
+    cmake --build $BUILD_DIR
 
-echo "==> Rodando testes"
-ctest --test-dir $BUILD_DIR
+    echo "==> Rodando testes"
+    ctest --test-dir $BUILD_DIR
+else
+    echo "o diretório $BUILD_DIR não existe"
+fi
