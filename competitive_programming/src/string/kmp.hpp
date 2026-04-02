c8be2a6bc173b7b67cfbc3d5c4dc7196fbfa97be
ThiagoFBastos <thiagobastos1705@gmail.com>
2026-04-02 10:43:34 -0300

    test: tests for the KMP algorithm

--- competitive_programming/src/string/kmp.hpp
+++ competitive_programming/src/string/kmp.hpp
@@ -8,6 +8,10 @@ namespace strings {
 inline std::vector<int> kmp(const std::string& s) 
 {
 	int n = s.size();
+
+	if(n == 0)
+		return {};
+		
 	std::vector<int> p(n);
 	p[0] = 0;
 	for(int i = 1; i < n; ++i) {
