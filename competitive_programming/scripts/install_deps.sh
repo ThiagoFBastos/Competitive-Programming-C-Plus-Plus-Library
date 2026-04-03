cd6d9c64898110fad7fd1db69615c71f0a129890
ThiagoFBastos <thiagobastos1705@gmail.com>
2026-04-03 18:03:17 -0300

    feat: enable script to be called from anywhere

--- competitive_programming/scripts/install_deps.sh
+++ competitive_programming/scripts/install_deps.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-BUILD_DIR=build
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+BUILD_DIR=$DIR/../build
 BUILD_TYPE=Release
+SRC=$DIR/..
+
+cd $SRC
 
 echo "==> Instalando dependências (Conan)"
 conan install . \
@@ -12,6 +17,6 @@ conan install . \
   -s build_type=$BUILD_TYPE
 
 echo "==> Configurando projeto (CMake)"
-cmake -S . -B $BUILD_DIR \
+cmake -S $SRC -B $BUILD_DIR \
   -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan_toolchain.cmake \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE
