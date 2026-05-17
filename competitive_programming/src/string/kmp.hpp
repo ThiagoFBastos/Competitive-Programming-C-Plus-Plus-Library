8a232a7d9113f58b4694fa0f28864d8a8779254f
Thiago Bastos <thiagobastos1705@gmail.com>
2026-05-17 12:06:08 -0300

    refactor: make some improvements to the code of KMP algorithm

--- competitive_programming/src/string/kmp.hpp
+++ competitive_programming/src/string/kmp.hpp
@@ -5,23 +5,32 @@
 
 namespace strings {
 
+/**
+ * @brief KMP algorithm
+ */
 inline std::vector<int> kmp(const std::string& s) 
 {
-	int n = s.size();
+	const auto n = static_cast<int>(s.size());
 
 	if(n == 0)
 		return {};
 		
 	std::vector<int> p(n);
+
 	p[0] = 0;
-	for(int i = 1; i < n; ++i) {
+	
+	for(int i = 1; i < n; ++i)
+	{
 		int j = p[i - 1];
+	
 		while(j && s[i] != s[j]) j = p[j - 1];
+	
 		p[i] = j;
+	
 		if(s[i] == s[j]) ++p[i];
 	}
+
 	return p;
 }
-
 }
 
